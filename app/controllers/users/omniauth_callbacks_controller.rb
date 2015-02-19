eee3117b599b166bbd65cf7285d4e455e16927e7
Álvaro <096.acc@gmail.com>
2015-02-19 22:03:31 +0100

    Fixed Callbacks Controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,18 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
       # You need to implement the method below in your model (e.g. app/models/user.rb)
       @user = User.from_omniauth(request.env["omniauth.auth"].except("extra"))
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
-  end
+      
+      if @user.persisted?
+        sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+        set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
+      else
+        session["devise.twitter_data"] = request.env["omniauth.auth"].except("extra")
+        @user.email    = 'change@me.please'
+        @user.password = Devise.friendly_token[0,20]
+        @user.username = auth.nickname
+        @user.role     ||= Role.find_by_name('registered')
+        @user.save
+        sign_in_and_redirect @user, :event => :authentication
+      end
+    end
 end
