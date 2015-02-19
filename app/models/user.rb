3caa7d0ef515c1ec9c73b7d8c9b1176cd2a26c5c
Álvaro <096.acc@gmail.com>
2015-02-19 22:00:48 +0100

    Fixed Callbacks Controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,6 @@ class User < ActiveRecord::Base
       user.password = Devise.friendly_token[0,20]
       user.username = auth.nickname
       user.role     ||= Role.find_by_name('registered')
-      user.save
       #user.image = auth.info.image # assuming the user model has an image
     end
   end
