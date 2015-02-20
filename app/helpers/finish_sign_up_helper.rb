bc32af8c55b5b8a396462f1109dec81a90f72cac
Álvaro <096.acc@gmail.com>
2015-02-20 22:57:00 +0100

    update user profiles without password

---
+++ app/helpers/finish_sign_up_helper.rb
@@ -0,0 +1,2 @@
+module FinishSignUpHelper
+end
---
+++ test/controllers/finish_sign_up_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FinishSignUpControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
