119d519b69b9c1255695af97db862309f9ed550d
Álvaro <096.acc@gmail.com>
2015-02-20 22:05:04 +0100

    generate test email for twitter signups

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -18,11 +18,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
     
    def update_resource(resource, params)
-       if(current_user.provider == 'twitter')
-         params.delete("current_password")
          resource.update_without_password(params)
-       else
-         resource.update_with_password(params)
-       end
-     end
+   end
 end
