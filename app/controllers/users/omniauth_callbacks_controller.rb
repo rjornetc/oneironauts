7b47cbde60f58c957832d8b93edef98da6965654
Álvaro <096.acc@gmail.com>
2015-02-20 22:00:05 +0100

    generate test email for twitter signups

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -16,4 +16,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         sign_in_and_redirect @user, :event => :authentication
       end
     end
+    
+   def update_resource(resource, params)
+       if current_user.provider
+         params.delete("current_password")
+         resource.update_without_password(params)
+       else
+         resource.update_with_password(params)
+       end
+     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
           sessions: 'users/sessions',
           omniauth_callbacks: 'users/omniauth_callbacks'
-  }
+  } 
   
   resources :widgets
 
