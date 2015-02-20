6efb583a20732fe5cad58a9c0a281073dd3bd456
Álvaro <096.acc@gmail.com>
2015-02-20 17:24:39 +0100

    generate test email for twitter signups

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,7 @@ class User < ActiveRecord::Base
   
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email    = auth.nickname+'@change.me'
+      user.email    = auth.info.nickname+'@change.me'
       user.password = Devise.friendly_token[0,20]
       user.username = auth.nickname
       user.role     ||= Role.find_by_name('registered')
