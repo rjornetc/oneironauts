63647002cc7cb0b0f99a930c9bd24c4ab64efb16
Álvaro <096.acc@gmail.com>
2015-02-22 13:41:16 +0100

    profile and gravatar fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,7 @@ class User < ActiveRecord::Base
   include Gravtastic
   gravtastic
   
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment :avatar, :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
          
   belongs_to :role
   
