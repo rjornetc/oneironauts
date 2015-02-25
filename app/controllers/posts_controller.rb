738e11f48a303bb4fa376c5f42644b6786c525ac
Álvaro <096.acc@gmail.com>
2015-02-25 23:40:35 +0100

    basic post controller variables

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,28 +1,42 @@
 class PostsController < ApplicationController
   def index
+      @posts = Post.all
   end
 
   def new
+      @post = Post.new
   end
 
   def create
+      @post = Post.new(post_params)
   end
 
   def edit
+      @post = Post.find(params[:id])
   end
 
   def update
+      @post = Post.find(params[:id])
   end
 
   def delete
+      @post = Post.find(params[:id])
   end
 
   def destroy
+      @post = Post.find(params[:id])
   end
 
   def vote_up
+      @post = Post.find(params[:id])
   end
 
   def vote_down
+      @post = Post.find(params[:id])
   end
+  
+  private
+      def post_params
+          params.require(:post).permit(:title, :content, :votes)
+      end
 end
