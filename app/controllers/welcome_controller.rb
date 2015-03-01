38813c959dbeb61401d45039db83a11650a4a877
Álvaro <096.acc@gmail.com>
2015-03-01 23:36:45 +0100

    fixes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,16 +5,6 @@ class WelcomeController < ApplicationController
 
   end
 
-    def resource_name
-        :user
-    end
     
-    def resource
-        @resource ||= User.new
-    end
-    
-    def devise_mapping
-        @devise_mapping ||= Devise.mappings[:user]
-    end
 
 end
