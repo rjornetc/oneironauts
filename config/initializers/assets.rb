d18d0a2cacff0d033505a04acc43600d5aa9df44
Álvaro <096.acc@gmail.com>
2015-03-08 14:08:36 +0100

    home page redesign

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-config.serve_static_assets = true
+Rails.application.config.serve_static_assets = true
