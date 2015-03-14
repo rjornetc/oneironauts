8d5b5ed43f7ee3112336e38bd76c14d126b0e752
Álvaro <096.acc@gmail.com>
2015-03-14 13:12:29 +0100

    badges fixes

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -2,7 +2,7 @@ class Achievement < ActiveRecord::Base
   has_many :achieved, dependent: :destroy
   has_many :users, through: :achieved
   
-  has_attached_file :icon, :styles => { :medium => "300x300>", :thumb => "100x100>" }, s3_permissions: :private,
+  has_attached_file :icon, :styles => { :medium => "300x300>", :thumb => "100x100>" },
                        s3_server_side_encryption: :aes256
   validates_attachment_content_type :icon, :content_type => /\Aimage\/.*\Z/
 end
